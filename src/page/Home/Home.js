3919cc32a278425868c8bfdafb0ab2d66a4f4762
xuejian.xu <jearbilove@yeah.net>
2018-01-31 21:19:27 +0800

    format

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -1,17 +1,15 @@
 import './Home.scss'
-import {h, Component} from 'preact'
+import { h, Component } from 'preact'
 import Month from './modules/Month/Month'
 import Day from './modules/Day/Day'
 
-class Home extends Component{
-    render(){
+class Home extends Component {
+    render() {
         return (
             <div className="g-page-home">
                 <Month />
                 <Day />
-                <div className="content-main">
-                    todolists
-                </div>
+                <div className="content-main">todolists</div>
             </div>
         )
     }
