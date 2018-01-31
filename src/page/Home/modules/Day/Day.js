f8bfc29d81b844d64ffeae3db82fd8fe71b2c636
xuxuejian <jearbilove@yeah.net>
2018-01-31 16:20:49 +0800

    add iscroll

--- src/page/Home/modules/Day/Day.js
+++ src/page/Home/modules/Day/Day.js
@@ -1,24 +1,38 @@
 import './Day.scss'
 import { h, Component } from 'preact'
+import IScroll from 'iscroll/build/iscroll-lite'
 
 const days = 31
 
 class Day extends Component {
+    domDays
+    componentDidMount() {
+        new IScroll(this.domDays)
+    }
     renderDays = () => {
         const doms = []
 
         for (let i = 1; i <= days; i++) {
             doms.push(
-                <div key={i} className="item">
+                <li key={i} className="item">
                     {i}
-                </div>
+                </li>
             )
         }
 
         return doms
     }
     render() {
-        return <div className="m-day">{this.renderDays()}</div>
+        return (
+            <div
+                className="m-day"
+                ref={dom => {
+                    this.domDays = dom
+                }}
+            >
+                <ul>{this.renderDays()}</ul>
+            </div>
+        )
     }
 }
 
