5ffb20c8a60ee32814d1aa2a5d4997187f2c801c
xuxuejian <jearbilove@yeah.net>
2018-02-01 14:54:15 +0800

    remove rxjs

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -2,27 +2,9 @@ import './Home.scss'
 import { h, Component } from 'preact'
 import Month from './modules/Month/Month'
 import Day from './modules/Day/Day'
-import Rx from 'rxjs/Rx'
 
 class Home extends Component {
-    observer
-    componentDidMount() {
-        const foo = Rx.Observable.create(() => {
-            const intervalId = setInterval(() => {
-                console.log(new Date().toLocaleTimeString())
-            }, 1000)
-
-            return () => {
-                console.log('clear interval');
-                clearInterval(intervalId)
-            }
-        })
-
-        this.observer = foo.subscribe()
-    }
-    onButtonClick = () => {
-        this.observer.unsubscribe()
-    }
+    onButtonClick = () => {}
     render() {
         return (
             <div className="g-page-home">
@@ -30,11 +12,7 @@ class Home extends Component {
                 <Day />
                 <div className="content-main">
                     <h1>Content Main</h1>
-                    <button
-                        onClick={this.onButtonClick}
-                    >
-                        Click me
-                    </button>
+                    <button onClick={this.onButtonClick}>Click me</button>
                 </div>
             </div>
         )
