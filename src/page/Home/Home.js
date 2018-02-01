25cf515ddfaa177e2e2813162e36a65b7ea9c9e4
xuxuejian <jearbilove@yeah.net>
2018-02-01 15:58:30 +0800

    update

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -4,15 +4,21 @@ import Month from './modules/Month/Month'
 import Day from './modules/Day/Day'
 
 class Home extends Component {
-    onButtonClick = () => {}
     render() {
         return (
             <div className="g-page-home">
                 <Month />
                 <Day />
                 <div className="content-main">
-                    <h1>Content Main</h1>
-                    <button onClick={this.onButtonClick}>Click me</button>
+                    <div className="title">
+                        <button className="btn-add">添加项目</button>
+                    </div>
+                    <ul className="items">
+                        <li className="item">
+                            <input className="checkbox" type="checkbox"/>
+                            <span>lalalalalala</span>
+                        </li>
+                    </ul>
                 </div>
             </div>
         )
